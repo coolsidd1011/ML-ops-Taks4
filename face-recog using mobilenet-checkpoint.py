d207e42b2fcd44a67fc38b1d653566ad5ed4e59b
Siddharth Singh <63062709+coolsidd1011@users.noreply.github.com>
2020-06-26 12:40:44 +0530

    uploaded code

---
+++ face-recog using mobilenet-checkpoint.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[61]:
+
+
+from keras.applications import MobileNet
+
+# MobileNet was designed to work on 224 x 224 pixel input images sizes
+img_rows, img_cols = 224, 224 
+
+# Re-loads the MobileNet model without the top or FC layers
+MobileNet = MobileNet(weights = 'imagenet', 
+                 include_top = False, 
+                 input_shape = (img_rows, img_cols, 3))
+
+# Here we freeze the last 4 layers 
+# Layers are set to trainable as True by default
+for layer in MobileNet.layers:
+    layer.trainable = False
+    
+# Let's print our layers 
+for (i,layer) in enumerate(MobileNet.layers):
+    print(str(i) + " "+ layer.__class__.__name__, layer.trainable)
+
+
+# In[62]:
+
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, GlobalAveragePooling2D
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+
+num_classes = 4
+top_model = MobileNet.output
+top_model = GlobalAveragePooling2D()(top_model)
+top_model = Dense(1024,activation='relu')(top_model)
+top_model = Dense(768,activation='relu')(top_model)
+top_model = Dense(512,activation='relu')(top_model)
+top_model = Dense(num_classes,activation='softmax')(top_model)
+    
+model = Model(inputs = MobileNet.input, outputs = top_model)
+print(model.summary())
+
+
+# In[63]:
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_data_dir = 'C://Users//KIIT//Desktop//ML-ops//ml task-2//train'
+validation_data_dir = 'C://Users//KIIT//Desktop//ML-ops//ml task-2//validate'
+
+# Let's use some data augmentaiton 
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=45,
+      width_shift_range=0.3,
+      height_shift_range=0.3,
+      horizontal_flip=True,
+      fill_mode='nearest')
+ 
+validation_datagen = ImageDataGenerator(rescale=1./255)
+ 
+
+batch_size = 64
+ 
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=batch_size,
+        class_mode='categorical')
+ 
+validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+
+# In[64]:
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_data_dir = 'C://Users//KIIT//Desktop//ML-ops//ml task-2//train'
+validation_data_dir = 'C://Users//KIIT//Desktop//ML-ops//ml task-2//validate'
+
+# Let's use some data augmentaiton 
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=45,
+      width_shift_range=0.3,
+      height_shift_range=0.3,
+      horizontal_flip=True,
+      fill_mode='nearest')
+ 
+validation_datagen = ImageDataGenerator(rescale=1./255)
+ 
+# set our batch size (typically on most mid tier systems we'll use 16-32)
+batch_size = 32
+ 
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=batch_size,
+        class_mode='categorical')
+ 
+validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+
+# In[67]:
+
+
+from keras.optimizers import RMSprop
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+
+                     
+checkpoint = ModelCheckpoint("faceRec.h5",
+                             monitor="val_loss",
+                             mode="min",
+                             save_best_only = True,
+                             verbose=1)
+
+earlystop = EarlyStopping(monitor = 'val_loss', 
+                          min_delta = 0, 
+                          patience = 3,
+                          verbose = 1,
+                          restore_best_weights = True)
+
+callbacks = [earlystop, checkpoint]
+
+# We use a very small learning rate 
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = RMSprop(lr = 0.001),
+              metrics = ['accuracy'])
+
+nb_train_samples = 42
+nb_validation_samples = 28
+
+# We only train 5 EPOCHS 
+epochs = 5
+
+batch_size = 16
+history = model.fit_generator(train_generator, epochs=3,steps_per_epoch=20, validation_data=validation_generator)
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch = nb_train_samples // batch_size,
+    epochs = epochs,
+    callbacks = callbacks,
+    validation_data = validation_generator,
+    validation_steps = nb_validation_samples // batch_size)
+
+
+# In[68]:
+
+
+from keras.models import load_model
+
+classifier = load_model('faceRec.h5')
+class_labels = validation_generator.class_indices
+class_labels = {v: k for k, v in class_labels.items()}
+classes = list(class_labels.values())
+print(classes)
+
+
+# In[82]:
+
+
+import os
+import cv2
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+from keras.preprocessing import image
+import re
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.metrics import classification_report, confusion_matrix
+import numpy as np
+
+def getRandomImage(path, img_width, img_height):
+    """function loads a random images from a random folder in our validation path """
+    folders = list(filter(lambda x: os.path.isdir(os.path.join(path, x)), os.listdir(path)))
+    random_directory = np.random.randint(0,len(folders))
+    path_class = folders[random_directory]
+    file_path = path + path_class
+    file_names = [f for f in listdir(file_path) if isfile(join(file_path, f))]
+    random_file_index = np.random.randint(0,len(file_names))
+    image_name = file_names[random_file_index]
+    final_path = file_path + "/" + image_name
+    return image.load_img(final_path, target_size = (img_width, img_height)), final_path, path_class
+
+# dimensions of our images
+img_width, img_height = 224, 224
+
+files = []
+predictions = []
+true_labels = []
+
+# predicting images
+for i in range(0, 5):
+    path = 'C://Users//KIIT//Desktop//ML-ops//ml task-2//validate//' 
+    img, final_path, true_label = getRandomImage(path, img_width, img_height)
+    files.append(final_path)
+    true_labels.append(true_label)
+    x = image.img_to_array(img)
+    x = x * 1./255
+    x = np.expand_dims(x, axis=0)
+    images = np.vstack([x])
+    classes = np.argmax(classifier.predict(images, batch_size = 8))
+    predictions.append(classes)
+    
+for i in range(0, len(files)):
+    img=mpimg.imread((files[i]))
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
+   # print("Predicted Image : ",class_labels[predictions[i]])
+    print("Actual Image: ",true_labels[i])
+    print("\n")
+
+
+# In[81]:
+
+
+classifier1 = load_model('faceRec.h5')
+def fine_tuning(path):
+    img = mpimg.imread(path)
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
+    
+    # prediction
+    img = image.load_img(path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = x.astype('float32')/255
+    pred = np.argmax(classifier.predict(x))
+    pred1 = np.argmax(classifier1.predict(x))
+    print("This is {}.:".format(class_labels[pred1]))
+    
+    
+fine_tuning('C://Users//KIIT//Desktop//ML-ops//ml task-2//validate//pic0.jpg')
+
+
+# In[ ]:
+
+
+
+
